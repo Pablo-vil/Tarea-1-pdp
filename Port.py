a9923f5f92d00266780515a7300acdec3d5fcce4
Pablo <pvvillagran@miuandes.cl>
2025-03-28 21:24:19 -0300

    small update

--- Port.py
+++ Port.py
@@ -2,3 +2,9 @@ class Port:
     def __init__ (self, id: str, material: str):
         self.id: str = id
         self.material = material
+
+    def get_id(self) -> str:
+        return self.id
+    
+    def get_material(self) -> str:
+        return self.material
--- Tile.py
+++ Tile.py
@@ -2,4 +2,11 @@ class Tile:
     def __init__(self, id: str, material: str, edges: dict):
         self.id: str = id
         self.material: str = material
-        self.edges: dict[str: str] = edges
+        self.edges: dict[str: str] = edges  
+
+    def get_id(self) -> str:
+        return self.id
+    def get_material(self) -> str:
+        return self.material
+    def get_edges(self) -> dict:
+        return self.edges
